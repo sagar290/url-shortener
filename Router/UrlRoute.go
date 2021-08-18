bd06de0035c232930f0ca45a53cc647318bcee90
sagar.dash290 <sagar.dash290@gmail.com>
2021-08-19 03:11:59 +0600

    Update UrlRoute.go

--- Router/UrlRoute.go
+++ Router/UrlRoute.go
@@ -13,6 +13,9 @@ func RegisterUrlsRoute(r *gin.Engine) {
 	url.Use(middleware.AuthRequired())
 	{
 		url.GET("/urls", Controller.GetUrls)
+		url.GET("/urls/:url_id", Controller.GetUrl)
+		url.PUT("/urls/:url_id", Controller.UpdateUrl)
+		url.DELETE("/urls/:url_id", Controller.DeleteUrl)
 		url.POST("/urls", Controller.AddUrl)
 	}
 
