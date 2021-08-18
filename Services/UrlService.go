d5094d39913376d07cd87222f1535f09cc461df2
sagar.dash290 <sagar.dash290@gmail.com>
2021-08-19 03:12:02 +0600

    Update UrlService.go

--- Services/UrlService.go
+++ Services/UrlService.go
@@ -16,6 +16,7 @@ func StoreUrl(url structs.UrlBody) (bool, string) {
 	dbc := models.Db.Create(&models.Url{
 		User_id:      int64(User.ID),
 		Redirect_url: url.Redirect_url,
+		Slug:         url.Slug,
 	})
 
 	if dbc.Error != nil {
@@ -42,7 +43,7 @@ func GetUrls() []models.Url {
 
 }
 
-func GetUrl(url_id int64) models.Url {
+func GetUrl(url_id int) models.Url {
 	var url models.Url
 
 	result := models.Db.Where(&models.Url{Url_id: uint(url_id)}).First(&url)
@@ -52,5 +53,49 @@ func GetUrl(url_id int64) models.Url {
 	}
 
 	return url
+
+}
+
+func UpdateUrl(url_id int, body structs.UrlBody) (bool, string) {
+	// var url models.Url
+	var message string
+
+	if body.Redirect_url != "" {
+
+		result := models.Db.Where(&models.Url{Url_id: uint(url_id)}).Update("redirect_url", body.Redirect_url)
+		if result.Error != nil {
+			message = "cant update"
+			return false, message
+		}
+	}
+
+	if body.Slug != "" {
+
+		result := models.Db.Where(&models.Url{Url_id: uint(url_id)}).Update("slug", body.Slug)
+		if result.Error != nil {
+			message = "cant update"
+			return false, message
+		}
+	}
+
+	message = "updated"
+
+	return true, message
+
+}
+
+func DeleteUrl(url_id int) (bool, string) {
+	var url models.Url
+	var message string
+
+	result := models.Db.Where(&models.Url{Url_id: uint(url_id)}).Delete(&url)
+	if result.Error != nil {
+		message = "cant delete"
+		return false, message
+	}
+
+	message = "deleted"
+
+	return true, message
 
 }
