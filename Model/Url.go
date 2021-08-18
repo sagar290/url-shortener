b437abc152ae5f6ca7e53936f1b1c3cd85328526
sagar.dash290 <sagar.dash290@gmail.com>
2021-08-19 03:11:57 +0600

    Update Url.go

--- Model/Url.go
+++ Model/Url.go
@@ -7,6 +7,7 @@ import (
 type Url struct {
 	Url_id       uint      `gorm:"primaryKey" json:"url_id"`
 	User_id      int64     `json:"user_id"`
+	Slug         string    `json:"slug"`
 	Redirect_url string    `gorm:"size:255" json:"redirect_url"`
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
