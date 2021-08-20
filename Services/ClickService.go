e03b7a57fbc700730060a6d6e18efd194e27daaf
sagar.dash290 <sagar.dash290@gmail.com>
2021-08-20 23:16:54 +0600

    Update ClickService.go

--- Services/ClickService.go
+++ Services/ClickService.go
@@ -6,10 +6,14 @@ import (
 
 func AddClick(url_id int, refer_type string) {
 	var click models.Click
+	var url models.Url
 	models.Db.Where(models.Click{Url_id: url_id, Referer: refer_type}).Assign(models.Click{Count: 0}).FirstOrCreate(&click)
+	models.Db.Where(models.Url{Url_id: uint(url_id)}).FirstOrCreate(&url)
 
 	count := click.Count + 1
+	total_count := url.TotalCount + 1
 	// fmt.Println(click.Click_id)
 	// fmt.Println(count)
 	models.Db.Model(&models.Click{}).Where(&models.Click{Click_id: click.Click_id}).Update("count", count)
+	models.Db.Model(&models.Url{}).Where(&models.Url{Url_id: url.Url_id}).Update("total_count", total_count)
 }
