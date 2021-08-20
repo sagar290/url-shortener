dd6ca431ece5834e5a4aeb50a543e953c2c08964
sagar.dash290 <sagar.dash290@gmail.com>
2021-08-20 23:16:51 +0600

    Update Url.go

--- Model/Url.go
+++ Model/Url.go
@@ -8,6 +8,7 @@ type Url struct {
 	Url_id       uint      `gorm:"primaryKey" json:"url_id"`
 	User_id      int64     `json:"user_id"`
 	Slug         string    `json:"slug"`
+	TotalCount   int       `json:"total_count"`
 	Redirect_url string    `gorm:"size:255" json:"redirect_url"`
 	Clicks       []Click   `gorm:"foreignKey:Url_id;references:Url_id"`
 	CreatedAt    time.Time `json:"created_at"`
